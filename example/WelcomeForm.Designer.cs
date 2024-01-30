ad6c982d71f14f211dd83a4b726e6fb7a0e17b3b
Huseyin Kocaturk <kocaturk4145@gmail.com>
2024-01-30 15:53:48 +0300

    Üff

--- example/WelcomeForm.Designer.cs
+++ example/WelcomeForm.Designer.cs
@@ -201,7 +201,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(159, 573);
+            this.button1.Location = new System.Drawing.Point(158, 533);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(221, 40);
             this.button1.TabIndex = 20;
