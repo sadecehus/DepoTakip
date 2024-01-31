c284de69d512db57934535d08d95b14a49043a0a
Huseyin Kocaturk <kocaturk4145@gmail.com>
2024-01-31 14:54:41 +0300

    14.54 31 ocak 24

--- example/Properties/Resources.Designer.cs
+++ example/Properties/Resources.Designer.cs
@@ -89,5 +89,35 @@ namespace example.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Başlıksız_1 {
+            get {
+                object obj = ResourceManager.GetObject("Başlıksız-1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Başlıksız_11 {
+            get {
+                object obj = ResourceManager.GetObject("Başlıksız-11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Başlıksız_12 {
+            get {
+                object obj = ResourceManager.GetObject("Başlıksız-12", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- example/WelcomeForm.Designer.cs
+++ example/WelcomeForm.Designer.cs
@@ -43,7 +43,6 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label6 = new System.Windows.Forms.Label();
             this.totalCountLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.productnamelabel = new System.Windows.Forms.Label();
             this.productcountlabel = new System.Windows.Forms.Label();
@@ -55,6 +54,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -199,17 +199,6 @@
             this.totalCountLabel.TabIndex = 19;
             this.totalCountLabel.Text = "-";
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(158, 533);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(221, 40);
-            this.button1.TabIndex = 20;
-            this.button1.Text = "Ürün Çeşidi Ekle";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
@@ -317,14 +306,27 @@
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button6.Location = new System.Drawing.Point(158, 607);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(221, 40);
             this.button6.TabIndex = 31;
             this.button6.Text = "Hasılat Ekle";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(158, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 40);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Ürün Çeşidi Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // WelcomeForm
             // 
